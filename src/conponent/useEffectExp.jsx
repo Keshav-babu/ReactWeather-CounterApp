25905771693c8f2ac4f1dc7d56b59a6ecf3d8019
Keshav-babu <keshavbabu742@gmail.com>
2022-03-25 20:52:37 +0530

    weather and counter app in react

---
+++ src/conponent/useEffectExp.jsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react"
+
+
+export const UseEffectExample=()=>{
+    const [count,setCount]=useState(10)
+    useEffect(()=>{
+        console.log(count,"I'll get invoke when the page mounts")
+    },[])
+    useEffect(()=>{
+        console.log(count,"I'll get invoked every time count changes " )
+    },[count])
+
+    return <>
+        <h3>Counter</h3>
+        <h5>Count : {count}</h5>
+        <button onClick={()=>{
+            setCount(count=>count+5)
+        }} >Change</button>
+    </>
+}
---
+++ src/conponent/weatherInfo.jsx
@@ -0,0 +1,55 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+
+const initialState={
+    query:"",
+    data:[]
+}
+export const WeatherInfo=()=>{
+
+    useEffect(()=>{
+        handleSearch()
+    },[])
+    const handleSearch=()=>{
+        //api call to weatherapi
+        //params
+        //api key 
+        //q
+        setLoading(true)
+        axios({
+            method:"get",
+            baseURL:"http://api.weatherapi.com/v1",
+            url:"/current.json",
+            params:{
+                key:"f87b2986947149e7b2a32245221003",
+                q:state.query||"Delhi"
+            }
+        }).then(res=>{
+            console.log(res.data)
+            setState({...state,data:res.data})
+            
+        }).catch(err=>console.log(err))
+        .finally(()=>setLoading(false))
+    }
+    const [state,setState]=useState(initialState)
+    const [loading,setLoading]=useState(false)
+
+    console.log("state",state)
+    return <>
+        <h1>Weather Information</h1>
+        <input type="text" onChange={(e)=>{
+            setState({...state,query:e.target.value})
+        }} />
+        <button onClick={handleSearch} >Show </button>
+        {loading && <p>...loading</p>}
+        {state?.data?.location &&
+            <h2>{state.data?.location?.name}</h2>
+        }
+        {state?.data?.current &&
+        <>
+            <h2>Current Cond : - {state?.data?.current?.condition?.text}</h2>
+            <img src={state?.data?.current?.condition?.icon} alt="weather logo" />
+        </>
+        }
+    </>
+}
